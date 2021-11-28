6fde3c7cac26236c6af7d5af37a87ad68b719b78
Retno Lestari <92641950+RetnoLestari@users.noreply.github.com>
2021-11-29 00:35:27 +0700

    Add files via upload

---
+++ Sekolah Artis.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t, b, u;
+    cin>> t >> b >> u;
+    
+    if (t>170 && b>55 && u>=15){
+    cout << "Lolos" <<endl;}
+    
+        else{
+    cout << "Tidak Lolos" <<endl;}
+    
+    return 0;
+}
